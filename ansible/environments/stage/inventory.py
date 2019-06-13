4f7d453b60687207e1b50266dcefecc734b5feb3
Bukharkin Sergei <serboox@gmail.com>
2019-06-14 01:10:44 +0300

    Fix in ansible dynamic inventory

--- ansible/environments/stage/inventory.py
+++ ansible/environments/stage/inventory.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding=utf8
 '''
 Example custom dynamic inventory script for Ansible, in Python.
 '''
@@ -6,9 +7,12 @@ Example custom dynamic inventory script for Ansible, in Python.
 import os
 import sys
 import argparse
-from keystoneauth1 import identity
+import re
+from keystoneauth1.identity import v3
 from keystoneauth1 import session
-from novaclient import client
+from keystoneclient.v3 import client as keystone_client
+from novaclient import client as nova_client
+from neutronclient.v2_0 import client as neutron_client
 
 try:
     import json
@@ -21,7 +25,59 @@ class ExampleInventory(object):
     def __init__(self):
         self._nova_client_version = 2
         self._inventory = {}
-        self.res = {'_meta': {'hostvars': {}}}
+        self.res = {
+            '_meta': {
+                'hostvars': {}
+            },
+            "all": {
+                "children": [
+                    "ungrouped",
+                    "srv",
+                    "postgres-cluster"
+                ],
+                "hosts": [],
+                "vars": {
+                    "ansible_python_interpreter": "/usr/bin/python3",
+                    "env": "stage",
+                    "pg_user": "postgres",
+                    "pg_version": 10
+                }
+            },
+            "srv": {
+                "children": [],
+                "hosts": [],
+                "vars": {}
+            },
+            "pg-master": {
+                "children": [],
+                "hosts": [],
+                "vars": {
+                    "role": "master"
+                }
+            },
+            "pg-slave": {
+                "children": [],
+                "hosts": [],
+                "vars": {
+                    "role": "slave"
+                }
+            },
+            "postgres-cluster": {
+                "children": [
+                    "pg-master",
+                    "pg-slave"
+                ],
+                "hosts": [],
+                "vars": {
+                    "ansible_ssh_common_args": "-o ProxyCommand=\"ssh -A -W %h:%p -q root@185.91.52.35\""
+                }
+            },
+            "ungrouped": {
+                "children": [],
+                "hosts": [],
+                "vars": {}
+            }
+        }
 
         self._set_environmet_variables()
         self._read_cli_args()
@@ -40,7 +96,7 @@ class ExampleInventory(object):
         else:
             self._inventory = self._empty_inventory()
 
-        print(json.dumps(self._inventory))
+        # print(json.dumps(self._inventory))
 
     def _set_environmet_variables(self):
         self._os_auth_url = os.getenv('OS_AUTH_URL')
@@ -66,79 +122,8 @@ class ExampleInventory(object):
         '''
         Example inventory for testing.
         '''
-        return {
-            "_meta": {
-                "hostvars": {
-                    "master1": {
-                        "ansible_host": "192.168.0.9",
-                    },
-                    "slave1": {
-                        "ansible_host": "192.168.0.6",
-                    },
-                    "slave2": {
-                        "ansible_host": "192.168.0.13",
-                    },
-                    "srv1": {
-                        "ansible_host": "185.91.52.35",
-                    }
-                }
-            },
-            "all": {
-                "children": [
-                    "ungrouped",
-                    "srv",
-                    "postgres-cluster"
-                ],
-                "hosts": [],
-                "vars": {
-                    "ansible_python_interpreter": "/usr/bin/python3",
-                    "env": "stage",
-                    "pg_user": "postgres",
-                    "pg_version": 10
-                }
-            },
-            "pg-master": {
-                "children": [],
-                "hosts": [
-                    "master1"
-                ],
-                "vars": {
-                    "role": "master"
-                }
-            },
-            "pg-slave": {
-                "children": [],
-                "hosts": [
-                    "slave1",
-                    "slave2"
-                ],
-                "vars": {
-                    "role": "slave"
-                }
-            },
-            "postgres-cluster": {
-                "children": [
-                    "pg-master",
-                    "pg-slave"
-                ],
-                "hosts": [],
-                "vars": {
-                    "ansible_ssh_common_args": "-o ProxyCommand=\"ssh -A -W %h:%p -q root@185.91.52.35\""
-                }
-            },
-            "srv": {
-                "children": [],
-                "hosts": [
-                    "srv1"
-                ],
-                "vars": {}
-            },
-            "ungrouped": {
-                "children": [],
-                "hosts": [],
-                "vars": {}
-            }
-        }
+        with open('inventory.json', mode='r') as json_file:
+            return json.load(json_file)
 
     def _empty_inventory(self):
         '''
@@ -151,33 +136,58 @@ class ExampleInventory(object):
         Generate custom Ansible inventory
         '''
         self._get_identity_session()
+        self._init_keystone_client()
         self._init_nova_client()
+        self._init_neutron_client()
 
         servers_list = self.nova.servers.list()
         for server in servers_list:
-            # print(server.__dict__.keys())
+            print(server.__dict__.keys())
             print('{id: %s, name: %s, addresses: %s}' % (
                 server.id,
                 server.name,
                 server.addresses['network_1'][0]['addr'],
             ))
-            self.res['_meta']['hostvars'].pop({server.name: {
+            self.res['_meta']['hostvars'][server.name] = {
                 'ansible_host': server.addresses['network_1'][0]['addr'],
-            }})
+            }
+
+            res = re.findall(r'srv', server.name)
+            if len(res) != 0:
+                self.res['srv']['hosts'].append(server.name)
+
+            res = re.findall(r'master', server.name)
+            if len(res) != 0:
+                self.res['pg-master']['hosts'].append(server.name)
+
+            res = re.findall(r'slave', server.name)
+            if len(res) != 0:
+                self.res['pg-slave']['hosts'].append(server.name)
+
+        print("\n\n")
+        print(self.keystone.users.list())
+        # print(self.neutron.list_subnets())
+
         return self.res
 
     def _get_identity_session(self):
-        auth = identity.V3Password(auth_url=self._os_auth_url,
-                                   username=self._os_username,
-                                   user_domain_name=self._os_user_domain_name,
-                                   password=self._os_password,
-                                   project_name=self._os_project_name,
-                                   project_domain_name=self._os_user_domain_name)
+        auth = v3.Password(auth_url=self._os_auth_url,
+                           username=self._os_username,
+                           user_domain_name=self._os_user_domain_name,
+                           password=self._os_password,
+                           project_name=self._os_project_name,
+                           project_domain_name=self._os_user_domain_name)
         self.session = session.Session(auth=auth)
 
+    def _init_keystone_client(self):
+        self.keystone = keystone_client.Client(session=self.session)
+
     def _init_nova_client(self):
-        self.nova = client.Client(
+        self.nova = nova_client.Client(
             self._nova_client_version, session=self.session)
+
+    def _init_neutron_client(self):
+        self.neutron = neutron_client.Client(session=self.session)
 
 
 ExampleInventory()
