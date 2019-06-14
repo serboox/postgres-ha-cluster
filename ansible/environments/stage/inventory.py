46feadb33b32513571eefdf8173cfc049e076295
Bukharkin Sergei <serboox@gmail.com>
2019-06-14 10:07:41 +0300

    Implements selectel dynamic inventory

--- ansible/environments/stage/inventory.py
+++ ansible/environments/stage/inventory.py
@@ -7,7 +7,9 @@ Example custom dynamic inventory script for Ansible, in Python.
 import os
 import sys
 import argparse
+import requests
 import re
+from urllib.parse import urlparse
 from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from keystoneclient.v3 import client as keystone_client
@@ -84,19 +86,16 @@ class ExampleInventory(object):
 
         # Called with `--list`.
         if self._args.list:
-            self._inventory = self._example_inventory()
+            self._inventory = self._build_selectel_inventory()
         # Called with `--host [hostname]`.
         elif self._args.host:
             # Not implemented, since we return _meta info `--list`.
             self._inventory = self._empty_inventory()
-        elif self._args.test:
-            # Not implemented, since we return _meta info `--list`.
-            self._inventory = self._openstack_inventory()
         # If no groups or vars are present, return an empty inventory.
         else:
-            self._inventory = self._empty_inventory()
+            self._inventory = self._build_selectel_inventory()
 
-        # print(json.dumps(self._inventory))
+        print(json.dumps(self._inventory))
 
     def _set_environmet_variables(self):
         self._os_auth_url = os.getenv('OS_AUTH_URL')
@@ -107,6 +106,7 @@ class ExampleInventory(object):
         self._os_password = os.getenv('OS_PASSWORD')
         self._os_region = os.getenv('OS_REGION')
         self._os_availability_zone = os.getenv('OS_AVAILABILITY_ZONE')
+        self._resell_url = os.getenv('RESELL_API_URL')
 
     def _read_cli_args(self):
         '''
@@ -115,7 +115,6 @@ class ExampleInventory(object):
         parser = argparse.ArgumentParser()
         parser.add_argument('--list', action='store_true')
         parser.add_argument('--host', action='store')
-        parser.add_argument('--test', action='store_true')
         self._args = parser.parse_args()
 
     def _example_inventory(self):
@@ -131,29 +130,42 @@ class ExampleInventory(object):
         '''
         return {'_meta': {'hostvars': {}}}
 
-    def _openstack_inventory(self):
+    def _build_selectel_inventory(self):
         '''
         Generate custom Ansible inventory
         '''
+        projects_list = self._find_projects_list()
+        for project in projects_list['projects']:
+            if project['name'] == self._os_project_name:
+                self._current_project = project
+
+        floatingip_list = self._find_floatingip_list()
+        for floatingip in floatingip_list['floatingips']:
+            if floatingip['project_id'] == self._current_project['id']:
+                self._current_floatingip = floatingip
+                break
+
         self._get_identity_session()
-        self._init_keystone_client()
         self._init_nova_client()
         self._init_neutron_client()
 
         servers_list = self.nova.servers.list()
         for server in servers_list:
-            print(server.__dict__.keys())
-            print('{id: %s, name: %s, addresses: %s}' % (
-                server.id,
-                server.name,
-                server.addresses['network_1'][0]['addr'],
-            ))
+            # print(server.__dict__.keys())
+            # print('{id: %s, name: %s, addresses: %s}' % (
+            #    server.id,
+            #    server.name,
+            #    server.addresses['network_1'][0]['addr'],
+            # ))
             self.res['_meta']['hostvars'][server.name] = {
                 'ansible_host': server.addresses['network_1'][0]['addr'],
             }
 
             res = re.findall(r'srv', server.name)
             if len(res) != 0:
+                self.res['_meta']['hostvars'][server.name] = {
+                    'ansible_host': self._current_floatingip['floating_ip_address'],
+                }
                 self.res['srv']['hosts'].append(server.name)
 
             res = re.findall(r'master', server.name)
@@ -163,11 +175,6 @@ class ExampleInventory(object):
             res = re.findall(r'slave', server.name)
             if len(res) != 0:
                 self.res['pg-slave']['hosts'].append(server.name)
-
-        print("\n\n")
-        print(self.keystone.users.list())
-        # print(self.neutron.list_subnets())
-
         return self.res
 
     def _get_identity_session(self):
@@ -179,8 +186,19 @@ class ExampleInventory(object):
                            project_domain_name=self._os_user_domain_name)
         self.session = session.Session(auth=auth)
 
-    def _init_keystone_client(self):
-        self.keystone = keystone_client.Client(session=self.session)
+    def _find_projects_list(self):
+        req_url = '%s/projects' % (self._resell_url)
+        # print(req_url)
+        headers = {'X-token': self._os_x_token}
+        r = requests.get(req_url, headers=headers)
+        return json.loads(r.text)
+
+    def _find_floatingip_list(self):
+        req_url = '%s/floatingips' % (self._resell_url)
+        # print(req_url)
+        headers = {'X-token': self._os_x_token}
+        r = requests.get(req_url, headers=headers)
+        return json.loads(r.text)
 
     def _init_nova_client(self):
         self.nova = nova_client.Client(
